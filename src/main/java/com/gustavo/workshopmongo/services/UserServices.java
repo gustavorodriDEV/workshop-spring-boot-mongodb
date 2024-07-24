d2bc61579312a9de2af7ee0354695056d1a9097a
gustavorogriguesoliveira012@gmail.com <gustavorodriguesoliveira012@gmail.com>
2024-07-24 16:44:01 -0300

    Inserção de usuário com POST

--- src/main/java/com/gustavo/workshopmongo/services/UserServices.java
+++ src/main/java/com/gustavo/workshopmongo/services/UserServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavo.workshopmongo.domain.User;
+import com.gustavo.workshopmongo.dto.UserDTO;
 import com.gustavo.workshopmongo.repository.UserRepository;
 import com.gustavo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserServices {
 	public User findBy(String id) {
 	    Optional<User> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(),objDTO.getEmail());
 	}
 
 }
