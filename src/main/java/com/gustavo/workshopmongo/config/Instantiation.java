522b23060dc323b4c93bae2f7469d5572ce605ae
gustavorogriguesoliveira012@gmail.com <gustavorodriguesoliveira012@gmail.com>
2024-07-24 23:09:20 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/gustavo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavo/workshopmongo/config/Instantiation.java
@@ -1,7 +1,6 @@
 package com.gustavo.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.gustavo.workshopmongo.domain.Post;
 import com.gustavo.workshopmongo.domain.User;
+import com.gustavo.workshopmongo.dto.AuthorDTO;
 import com.gustavo.workshopmongo.repository.PostRepository;
 import com.gustavo.workshopmongo.repository.UserRepository;
 
@@ -19,28 +19,30 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post = new Post(null,sdf.parse("21/03/2028"), "Partiu viagem", "Vou viajar para São paulo. Abroçoes!", maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2028"), "Bom dia", "Acordeu feliz hoje!", maria);
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		postRepository.saveAll(Arrays.asList(post, post));
-		
+
+		Post post = new Post(null, sdf.parse("21/03/2028"), "Partiu viagem", "Vou viajar para São paulo. Abroçoes!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2028"), "Bom dia", "Acordeu feliz hoje!", new AuthorDTO(maria));
+
+		postRepository.saveAll(Arrays.asList(post, post2));
+
 	}
 }
